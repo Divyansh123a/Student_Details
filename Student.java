c2994af28c7b9d5c4f43e00c1dff5a3bbe2777a4
Divyansh123a <43573674+Divyansh123a@users.noreply.github.com>
2021-06-15 12:34:38 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,37 @@
+
+public class Student {
+
+    // Fill the code here
+    private int studentId;
+    private String studentName , studentAddress , collegeName;
+    public Student(int studentId , String studentName , String studentAddress)
+    {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+        this.collegeName = "NIT";
+    }
+    public Student(int studentId , String studentName , String studentAddress , String collegeName)
+    {
+        this(studentId , studentName , studentAddress);
+        this.collegeName = collegeName;
+    }
+    public int getStudentId()
+    {
+        return this.studentId;
+    }
+    public String getStudentName()
+    {
+        return this.studentName;
+    }
+    public String getStudentAddress()
+    {
+        return this.studentAddress;
+    }
+    public String getCollegeName()
+    {
+        return this.collegeName;
+    }
+
+
+}
